e4f9a653b7417f7380969094357e0835fc9c6f4d
VitorEdgar <vitorsedgar@gmail.com>
2020-06-29 14:21:48 -0300

    porta

--- src/src/Main.java
+++ src/src/Main.java
@@ -51,7 +51,7 @@ public class Main {
         try {
             //Deve se cuidar para definir o hostlocal, senão acaba ocorrendo erro de conexão ao tentar interagir com o arquivo
             System.setProperty("java.rmi.server.hostname", nodo.address);
-            LocateRegistry.createRegistry(Integer.parseInt(nodo.port));
+            LocateRegistry.createRegistry(1099);
             System.out.println("java RMI registry created.");
         } catch (RemoteException e) {
             System.out.println("java RMI registry already exists.");
